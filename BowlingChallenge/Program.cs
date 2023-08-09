737a9a74b9050e070d1e6c0bd3fab6937d28236a
Pingan Cheng <pingancheng96@gmail.com>
2023-08-09 10:47:13 +0200

    update demonstration examples

--- BowlingChallenge/Program.cs
+++ BowlingChallenge/Program.cs
@@ -3,24 +3,27 @@
 BowlingScoreboard bowlingScoreboard = new BowlingScoreboard();
 
 // example game
+Console.WriteLine("Example Game:");
 List<int> exampleRolls = new() { 1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 1, 7, 3, 6, 4, 10, 2, 8, 6 };
 foreach (int roll in exampleRolls)
     bowlingScoreboard.RegisterRollToBoard(roll);
-Console.WriteLine(bowlingScoreboard.ToString());
+Console.Write(bowlingScoreboard.ToString());
 Console.WriteLine($"Total Score: {bowlingScoreboard.GetTotalScore()}\n");
 
 bowlingScoreboard.ClearScoreboard();
 
 // gutter game
+Console.WriteLine("Gutter Game:");
 RegisterRepeatedRolls(0, 20);
-Console.WriteLine(bowlingScoreboard.ToString());
+Console.Write(bowlingScoreboard.ToString());
 Console.WriteLine($"Total Score: {bowlingScoreboard.GetTotalScore()}\n");
 
 bowlingScoreboard.ClearScoreboard();
 
 // perfect game
+Console.WriteLine("Perfect Game:");
 RegisterRepeatedRolls(10, 12);
-Console.WriteLine(bowlingScoreboard.ToString());
+Console.Write(bowlingScoreboard.ToString());
 Console.WriteLine($"Total Score: {bowlingScoreboard.GetTotalScore()}\n");
 
 void RegisterRepeatedRolls(int pins, int rolls)
